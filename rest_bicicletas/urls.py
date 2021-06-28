1c7b46614ffbce930d9dea4ced9bcbdba511f339
CamiloGonzalezSoto <caj.gonzalezs@duocuc.cl>
2021-06-28 17:45:34 -0400

    se agrega seguridad

--- rest_bicicletas/urls.py
+++ rest_bicicletas/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from rest_bicicletas.views import lista_personas, detalle_bicicletas
+from rest_bicicletas.viewsLogin import login
 
 urlpatterns =[
     path('lista_personas', lista_personas, name="lista_personas"),
     path('detalle_bicicletas/<id>', detalle_bicicletas, name="detalle_bicicletas"),
+    path('login',login, name="login"),
 ]
 
--- rest_bicicletas/views.py
+++ rest_bicicletas/views.py
@@ -1,21 +1,27 @@
 from django.shortcuts import render
 from rest_framework import status
 from rest_framework.decorators import api_view
+from rest_framework.decorators import permission_classes
 from rest_framework.response import Response
 from rest_framework.parsers import JSONParser
 from django.views.decorators.csrf import csrf_exempt
 from core.models import Formulario_Persona
 from .serializers import PersonaSerializer
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+
 
 @csrf_exempt
 @api_view(['GET', 'POST'])
+@permission_classes((IsAuthenticated,))
 def lista_personas(request):
     if request.method == 'GET':
         persona = Formulario_Persona.objects.all()
         serializer = PersonaSerializer(persona, many=True)
         return Response(serializer.data)
     elif request.method == 'POST':
-        data = JSONParser().parser(request)
+        data = JSONParser().parse(request)
         serializer = PersonaSerializer(data=data)
         if serializer.is_valid():
             serializer.save()
@@ -23,9 +29,10 @@ def lista_personas(request):
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 @api_view(['GET', 'PUT', 'DELETE'])
+@permission_classes((IsAuthenticated,))
 def detalle_bicicletas(request, id):
     try:
-        persona = Formulario_Persona.objects.get(rut=id)
+        persona = Formulario_Persona.objects.get(id=id)
     except Formulario_Persona.DoesNotExist:
             return Response(status=status.HTTP_404_NOT_FOUND)
     if request.method == 'GET':
